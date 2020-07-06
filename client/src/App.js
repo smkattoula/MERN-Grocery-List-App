1d112fe54d5f6e9da2bb62e986a672e2c6ffc872
Shaker Kattoula <smkattoula@gmail.com>
2020-07-06 15:41:31 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import AppNavBar from './components/AppNavBar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/ItemModal';
+
+import { Container } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+    <div className="App">
+      <AppNavBar />
+      <Container>
+      <ItemModal />
+      <ShoppingList />
+      </Container>
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://smkattoula:SHka9092@shoppinglist.lzvdi.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
